4ad580a9f09ab154a3afeed66df6658dcaab0166
facundo_tasillo <tasillofacundo@gmail.com>
2023-01-13 16:58:51 -0300

    final

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import {useState, useEffect} from "react";
 import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
-import  ItemList from "./ItemeList";
-import Loader from "./Loadeer";
+import  ItemList from "./ItemList";
+import Loader from "./Loader";
 
 
 
